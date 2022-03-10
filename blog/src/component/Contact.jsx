dbb8abe0c11b155e9c247c9056b529e2d936fc28
Mücahit Kökdemir <76880536+mucahitkok@users.noreply.github.com>
2022-03-10 17:49:59 +0300

    contact bölümü tamamlandı

--- blog/src/component/Contact.jsx
+++ blog/src/component/Contact.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { useData } from "../Context/DataContext";
+import { IntlProvider, FormattedMessage } from "react-intl";
+import { contact } from "./lang/Langs";
 
 function Contact() {
+  const { data} = useData();
   return (
-    <div>Contact</div>
+    <div>
+      <IntlProvider locale={data} messages={contact[data]}> 
+      <h2><FormattedMessage id="reachMe"/></h2>
+      <ul className='d-flex contact'>
+        <li className='me-4 fs-1'><a href="https://github.com/mucahitkok" target="_blank" rel="noopener noreferrer"><i class="fa fa-github"></i></a></li>
+        <li className='me-4 fs-1'><a href="https://www.instagram.com/mucahitkok/" target="_blank" rel="noopener noreferrer"><i class="fa fa-instagram"></i></a></li>
+        <li className='me-4 fs-1'><a href="https://twitter.com/muccorn" target="_blank" rel="noopener noreferrer"><i class="fa fa-twitter"></i></a></li>
+        <li className='me-4 fs-1'><a href="https://www.linkedin.com/in/mucahitkok/" target="_blank" rel="noopener noreferrer"><i class="fa fa-linkedin"></i></a></li>
+      </ul>
+      </IntlProvider>
+    </div>
   )
 }
 
--- blog/src/component/lang/Langs.js
+++ blog/src/component/lang/Langs.js
@@ -1,31 +1,40 @@
-export  const nav = {
-    "tr-TR" : {
-      home : "Ana Sayfa",
-      about : "Hakkımda",
-      projects : "Projeler",
-      contact : "İletişim"
-    },
-    "en-US" : {
-      home : "Home",
-      about : "About",
-      projects : "Projects",
-      contact : "Contact"
-    }
+export const nav = {
+  "tr-TR": {
+    home: "Ana Sayfa",
+    about: "Hakkımda",
+    projects: "Projeler",
+    contact: "İletişim"
+  },
+  "en-US": {
+    home: "Home",
+    about: "About",
+    projects: "Projects",
+    contact: "Contact"
   }
+}
 
-export  const home = {
-    "tr-TR" : {
-        hii : "Merhaba, Ben Mücahit KÖKDEMİR",
-        bio : "Türkiye'den tutkulu Jr. Frontend Developer ",
-        learning : "Şuan React.js öğreniyorum",
-        askMe : "Bana html,css,js,react,vue hakkında soru sorabilirsin.",
-        reachMe : "Bana Mail yoluyla ulaşabilirsiniz"
-    },
-    "en-US" : {
-        hii : "Hi, I'm Mucahit KOKDEMIR",
-        bio : "A passionate Jr. frontend developer from Turkey",
-        learning : " I’m currently learning React.js",
-        askMe : "Ask me about html,css,js,react,vue",
-        reachMe : "You can reach me via Mail"
-    }
+export const home = {
+  "tr-TR": {
+    hii: "Merhaba, Ben Mücahit KÖKDEMİR",
+    bio: "Türkiye'den tutkulu Jr. Frontend Developer ",
+    learning: "Şuan React.js öğreniyorum",
+    askMe: "Bana html,css,js,react,vue hakkında soru sorabilirsin.",
+    reachMe: "Bana Mail yoluyla ulaşabilirsiniz"
+  },
+  "en-US": {
+    hii: "Hi, I'm Mucahit KOKDEMIR",
+    bio: "A passionate Jr. frontend developer from Turkey",
+    learning: " I’m currently learning React.js",
+    askMe: "Ask me about html,css,js,react,vue",
+    reachMe: "You can reach me via Mail"
   }
+}
+
+export const contact = {
+  "tr-TR": {
+    reachMe: "Bana ulaş"
+  },
+  "en-US": {
+    reachMe: "Reach me"
+  }
+}

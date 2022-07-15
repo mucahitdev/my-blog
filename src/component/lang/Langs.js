bfd68625b60d314e128a8284340aa6b06ba821ed
Mücahit Kökdemir <76880536+mucahitkok@users.noreply.github.com>
2022-07-15 22:39:34 +0300

    Yeni işimi ekledim.

--- src/component/lang/Langs.js
+++ src/component/lang/Langs.js
@@ -16,15 +16,15 @@ export const nav = {
 export const home = {
   "tr-TR": {
     hii: "Merhaba, Ben Mücahit KÖKDEMİR",
-    bio: "Türkiye'den tutkulu Jr. Frontend Developer ",
-    learning: "Şuan React.js öğreniyorum",
+    bio: "NTT DATA şirketinde Jr.Frontend Developer olarak çalışmaktayım.",
+    learning: "Şuan Frontend teknolojilerinde kendimi geliştirmekteyim.",
     askMe: "Bana html,css,js,react,vue hakkında soru sorabilirsin.",
     reachMe: "Bana Mail yoluyla ulaşabilirsiniz"
   },
   "en-US": {
     hii: "Hi, I'm Mucahit KOKDEMIR",
-    bio: "A passionate Jr. frontend developer from Turkey",
-    learning: " I’m currently learning React.js",
+    bio: "I am working as Jr.Frontend Developer at NTT DATA.",
+    learning: "I'm currently developing myself in Frontend technologies.",
     askMe: "Ask me about html,css,js,react,vue",
     reachMe: "You can reach me via Mail"
   }

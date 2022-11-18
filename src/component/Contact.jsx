4bb683fccb4739c3b879bade6bf498db5706ff76
Mücahit <76880536+mucahitdev@users.noreply.github.com>
2022-11-18 10:22:21 +0000

    changed social username

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -10,10 +10,10 @@ function Contact() {
       <IntlProvider locale={data} messages={contact[data]}> 
       <h2><FormattedMessage id="reachMe"/></h2>
       <ul className='d-flex contact'>
-        <li className='me-4 fs-1'><a href="https://github.com/mucahitkok" target="_blank" rel="noopener noreferrer"><i class="fa fa-github"></i></a></li>
-        <li className='me-4 fs-1'><a href="https://www.instagram.com/mucahitkok/" target="_blank" rel="noopener noreferrer"><i class="fa fa-instagram"></i></a></li>
-        <li className='me-4 fs-1'><a href="https://twitter.com/muccorn" target="_blank" rel="noopener noreferrer"><i class="fa fa-twitter"></i></a></li>
-        <li className='me-4 fs-1'><a href="https://www.linkedin.com/in/mucahitkok/" target="_blank" rel="noopener noreferrer"><i class="fa fa-linkedin"></i></a></li>
+        <li className='me-4 fs-1'><a href="https://github.com/mucahitdev" target="_blank" rel="noopener noreferrer"><i class="fa fa-github"></i></a></li>
+        <li className='me-4 fs-1'><a href="https://www.instagram.com/mucahitkdev/" target="_blank" rel="noopener noreferrer"><i class="fa fa-instagram"></i></a></li>
+        <li className='me-4 fs-1'><a href="https://twitter.com/mucahitdev" target="_blank" rel="noopener noreferrer"><i class="fa fa-twitter"></i></a></li>
+        <li className='me-4 fs-1'><a href="https://www.linkedin.com/in/mucahitdev/" target="_blank" rel="noopener noreferrer"><i class="fa fa-linkedin"></i></a></li>
       </ul>
       </IntlProvider>
     </div>

71797f79acbd57a2ed497a450008afd9150b60be
Mücahit <76880536+mucahitdev@users.noreply.github.com>
2022-11-18 10:27:19 +0000

    ..

--- src/component/data/porjectsData/index.js
+++ src/component/data/porjectsData/index.js
@@ -10,7 +10,7 @@ export const projectsData = [
         name: "Nöbetçi Eczane",
         description: "Türkiyedeki Nöbetçi Eczaneleri İl ve İlçe bazlı bulabilceğiniz Uygulama.",
         demo: "https://nobetci-eczaneniz.netlify.app",
-        code: "https://github.com/mucahitkok/nobetci-eczane",
+        code: "https://github.com/mucahitdev/nobetci-eczane",
         image: "https://serving.photos.photobox.com/10561426f7732549d3d1a2fb67c4e2ed564eb01ac71800fea91f20bbaedcc0002a51da91.jpg",
         tags: [
             {
@@ -28,7 +28,7 @@ export const projectsData = [
         name: "Blog",
         description: "Kişisel blog sayfam.",
         demo: "https://mucahitkok.netlify.app",
-        code: "https://github.com/mucahitkok/my-blog",
+        code: "https://github.com/mucahitdev/my-blog",
         image: "https://serving.photos.photobox.com/026185021cf06a0725c119ae2b3cf6348d528fc149fad009bb2d7936c3818e14f010389e.jpg",
         tags: [
             {
@@ -43,7 +43,7 @@ export const projectsData = [
         name: "Colour palette",
         description: "Renk paletleri üretip yayınlayabilir,Başkalarının paletlerinide görebilirsiniz.",
         demo: "https://color-palet.vercel.app",
-        code: "https://github.com/mucahitkok/color-flipper-react",
+        code: "https://github.com/mucahitdev/color-flipper-react",
         image: "https://serving.photos.photobox.com/587576862d1e852132899a7e65b7feb3f40a0a4c73177d8b98841fae62e8b602d1f47ccc.jpg",
         tags: [
             {
@@ -61,7 +61,7 @@ export const projectsData = [
         name: "Bergenfy",
         description: "Bergen'in anısına yapmış olduğum, bazı müziklerini ve fotoğraflarını topladığım site.",
         demo: "https://bergenfy.netlify.app",
-        code: "https://github.com/mucahitkok/bergenfy",
+        code: "https://github.com/mucahitdev/bergenfy",
         image: "https://serving.photos.photobox.com/39099388b10602a0f15d87f852468ed57223a68afd2bd7d5743a58c4c2b7413450204b79.jpg",
         tags: [
             {
@@ -76,7 +76,7 @@ export const projectsData = [
         name: "Love Calculator",
         description: "İki isim arasındaki uyumu bir takım algoritmaya dayanarak hesaplayan app.",
         demo: "https://lovecalculator-tr.netlify.app/",
-        code: "https://github.com/mucahitkok/love-colculator",
+        code: "https://github.com/mucahitdev/love-colculator",
         image: "https://serving.photos.photobox.com/629407374e99849d3ce91578780ebe43d97772f79c15cdf2c76c7401876889f7842bdbb5.jpg",
         tags: [
             {
@@ -91,7 +91,7 @@ export const projectsData = [
         name: "Mikail App",
         description: "Hava durumu uygulaması.",
         demo: "https://mikail-weather.netlify.app",
-        code: "https://github.com/mucahitkok/mikail-app",
+        code: "https://github.com/mucahitdev/mikail-app",
         image: `${mikailWeather}`,
         tags: [
             {
@@ -109,7 +109,7 @@ export const projectsData = [
         name: "Filmtika",
         description: "Trend filmler ve Film arama.",
         demo: "https://filmtika.netlify.app",
-        code: "https://github.com/mucahitkok/filmtika",
+        code: "https://github.com/mucahitdev/filmtika",
         image: `${Filmtika}`,
         tags: [
             {
@@ -127,7 +127,7 @@ export const projectsData = [
         name: "Alo Junior Destek",
         description: "Linkedin ağınızı ilgi alanınıza göre genişletin.",
         demo: "https://alo-junior-destek.netlify.app",
-        code: "https://github.com/mucahitkok/alo-junior-destek",
+        code: "https://github.com/mucahitdev/alo-junior-destek",
         image: `${aloJuniorDestek}`,
         tags: [
             {

552c43cf066d30ab998f7dab557e92f4e1e47dda
Mücahit <76880536+mucahitdev@users.noreply.github.com>
2024-02-03 13:43:59 +0300

    Update Langs.js

--- src/component/lang/Langs.js
+++ src/component/lang/Langs.js
@@ -16,14 +16,14 @@ export const nav = {
 export const home = {
   "tr-TR": {
     hii: "Merhaba, Ben Mücahit KÖKDEMİR",
-    bio: "NTT DATA şirketinde Jr.Frontend Developer olarak çalışmaktayım.",
+    bio: "NTT DATA şirketinde Frontend & React Native Developer olarak çalışmaktayım.",
     learning: "Şuan Frontend teknolojilerinde kendimi geliştirmekteyim.",
     askMe: "Bana html,css,js,react,vue hakkında soru sorabilirsin.",
     reachMe: "Bana Mail yoluyla ulaşabilirsiniz"
   },
   "en-US": {
     hii: "Hi, I'm Mucahit KOKDEMIR",
-    bio: "I am working as Jr.Frontend Developer at NTT DATA.",
+    bio: "I am working as Frontend & React Native Developer at NTT DATA.",
     learning: "I'm currently developing myself in Frontend technologies.",
     askMe: "Ask me about html,css,js,react,vue",
     reachMe: "You can reach me via Mail"
